0fcb4c5ef4a3d17a354f362e4032a19096995b1c
rasheedawilson12 <rasheedawilson12@gmail.com>
2023-06-22 13:25:05 -0500

    Some logic fixes and debugging...I'm mostly back where I was yesterday

--- app.js
+++ app.js
@@ -1,101 +1,167 @@
 // Global Variables
 let sucessfulHit = Math.floor(Math.random() * (10, 1) + 1);
-
-let round = 1;
-let i = 0;
-let j = 0;
+const getRandomNumber = (min, max) => {
+  let randomNumber = Math.floor(Math.random() * (max - min) + min);
+  return randomNumber;
+};
 
 // Query Selectors
 // "Global"
 let body = document.querySelector("body");
 let gameHeader = document.querySelector(".gameHeader");
 let currentRound = document.querySelector(".roundHeader");
+let feed = document.querySelector(".feed");
 // Player One
 let playOneHeader = document.querySelector(".playerOneHeader");
 let playerOneName = document.querySelector(".playerOneName");
 playerOneHealth = document.querySelector(".playerOneHealth");
-let playerOneFeed = document.querySelector(".playerOneFeed");
+let heroImg = document.querySelector(".hero");
 // Player Two
 let playerTwoHeader = document.querySelector(".playerTwoHeader");
 let playerTwoName = document.querySelector(".playerTwoName");
 playerTwoHealth = document.querySelector(".playerTwoHealth");
-let playerTwoFeed = document.querySelector(".playerTwoFeed");
-let alien = document.querySelector(".alien");
+let alienImg = document.querySelector(".alien");
 
 // Player Objects
 let playerOne = {
   name: "USS Assembly",
-  health: 20,
-  firepower: function firepower(min, max) {
-    return Math.floor(Math.random() * (max - min) + min);
-  },
-  accuracy: function accuracy(min, max) {
-    return Math.floor(Math.random() * (max - min) + min);
-  },
+  hull: 20,
+  firepower: 5,
+  accuracy: 7,
   turnToAttack: true,
+  image:
+    "https://media.tenor.com/oHgHEzWWAFQAAAAC/space-adventure-cobra-spaceship.gif",
 };
 
-let playerTwo = {
-  name: ["Kang", "Kodos", "Gazoo", "Zim", "Roger", "Zoidburg"],
-  health: 20,
-  firepower: function firepower(min, max) {
-    return Math.floor(Math.random() * (max - min) + min);
-  },
-  accuracy: function accuracy(min, max) {
-    return Math.floor(Math.random() * (max - min) + min);
-  },
+let alienOne = {
+  name: "Kang & Konos",
+  hull: getRandomNumber(3, 6),
+  firepower: getRandomNumber(2, 4),
+  accuracy: getRandomNumber(6, 8),
   turnToAttack: false,
-  image: [
-    "https://media.tenor.com/RlxrgBXn_tMAAAAd/alien-attack-war-of-the-worlds.gif",
-    "https://i.pinimg.com/originals/e7/40/2c/e7402cbdb59722ba7e226b63748d1a69.gif",
-    "https://cdn3.whatculture.com/images/2014/08/aliens-colonial-marines-dance-600x338.gif",
-    "https://townsquare.media/site/442/files/2018/03/infinitywargif5.gif",
-    "https://media3.giphy.com/media/3o7TKsWZbKd2dVqqPu/200w.gif?cid=6c09b952kbdbvm0c4cnl80i64jhnxt7zqnxzo1h0afav0aru&ep=v1_gifs_search&rid=200w.gif&ct=g",
-    "https://cinemasiren.com/wp-content/uploads/2014/11/alien-gif.gif",
-  ],
+  image: "https://media.tenor.com/st8PxZ-amRsAAAAC/kang-kodos.gif",
 };
+let alienTwo = {
+  name: "Roger The Alien",
+  hull: getRandomNumber(3, 6),
+  firepower: getRandomNumber(2, 4),
+  accuracy: getRandomNumber(6, 8),
+  turnToAttack: false,
+  image:
+    "https://media1.giphy.com/media/XuApq8Jm12KIw/200w.gif?cid=6c09b952iemvqr46evemxfbmoghf246x713o05g27t61ecf2&ep=v1_gifs_search&rid=200w.gif&ct=g",
+};
+
+let alienThree = {
+  name: "Invader Zim",
+  hull: getRandomNumber(3, 6),
+  firepower: getRandomNumber(2, 4),
+  accuracy: getRandomNumber(6, 8),
+  turnToAttack: false,
+  image: "https://i.gifer.com/NYt7.gif",
+};
+
+let alienFour = {
+  name: "Dr. Zoidburg",
+  hull: getRandomNumber(3, 6),
+  firepower: getRandomNumber(2, 4),
+  accuracy: getRandomNumber(6, 8),
+  turnToAttack: false,
+  image: "https://media2.giphy.com/media/jbxQLpOKN2URa/giphy.gif",
+};
+
+let alienFive = {
+  name: "Lrrr",
+  hull: getRandomNumber(3, 6),
+  firepower: getRandomNumber(2, 4),
+  accuracy: getRandomNumber(6, 8),
+  turnToAttack: false,
+  image:
+    "https://pa1.aminoapps.com/6819/2a161e8efde898096cbce4391b0022b980a41ed0_hq.gif",
+};
+
+let alienSix = {
+  name: "Thanos",
+  hull: getRandomNumber(3, 6),
+  firepower: getRandomNumber(2, 4),
+  accuracy: getRandomNumber(6, 8),
+  turnToAttack: false,
+  image:
+    "https://pa1.aminoapps.com/6819/2a161e8efde898096cbce4391b0022b980a41ed0_hq.gif",
+};
+
+let alienScum = [
+  alienOne,
+  alienTwo,
+  alienThree,
+  alienFour,
+  alienFive,
+  alienSix,
+];
+let playerTwo = alienOne;
+let counter = 0;
+
+currentRound.innerHTML = `${counter}`;
+playerOneName.innerHTML = `${playerOne.name}`;
+playerTwoName.innerHTML = `${playerTwo.name}`;
+playerOneHealth.innerHTML = `${playerOne.hull}`;
+playerTwoHealth.innerHTML = `${playerTwo.hull}`;
+heroImg.setAttribute("src", playerOne.image);
+alienImg.setAttribute("src", playerTwo.image);
 
 // Function List
-
-const startRound = (player) => {
-  round = 1;
-  playerOneHealth.innerHTML = `${playerOne.health}`;
-  playerTwoHealth.innerHTML = `${playerTwo.health}`;
-  playerTwo.name.innerHTML = `${playerTwo.name[i]}`;
-  playerOneFeed.innerHTML = "";
-  playerTwoFeed.innerHTML = "";
-  attackPlayerTwo(player);
-  setTimeout(() => {
-    attackPlayerOne(player);
-  }, "6000");
-  checkHP(player);
+const heroHP = () => {
+  if (playerTwo.hull <= 0) {
+    alert("Game Over");
+  } else {
+    startRound(playerTwo);
+  }
 };
 
-const attackPlayerTwo = (player) => {
+const alienHP = () => {
+  if (playerTwo.hull <= 0) {
+    alert(`${playerTwo.name} has lost this round`);
+    switchAlien();
+    counter += 1;
+    playerTwo = alienScum[counter];
+  } else {
+    setTimeout(() => {
+      alienAttack();
+      heroHP();
+    }, "6000");
+  }
+};
+
+const startRound = () => {
+  heroAttack();
+  alienHP();
+};
+
+const heroAttack = () => {
   setTimeout(() => {
-    playerTwoFeed.innerHTML = `${playerOne.name} is preparing to fire!`;
+    feed.innerHTML = `${playerOne.name} is preparing to fire!`;
   }, "2000");
-  let currentLife = playerOne.health;
+  let currentLife = playerOne.hull;
   if (playerOne.turnToAttack === true) {
-    if (sucessfulHit < playerOne.accuracy(1, 10)) {
-      currentLife = playerTwo.health -= playerOne.firepower(1, 10);
+    if (sucessfulHit < playerOne.accuracy) {
+      currentLife = playerTwo.hull -= playerOne.firepower;
       setTimeout(() => {
-        playerTwoFeed.innerHTML = `${playerOne.name} has fired!`;
+        feed.innerHTML = `${playerOne.name} has fired!`;
       }, "3000");
       setTimeout(() => {
-        playerTwoFeed.innerHTML = `${playerOne.name}'s hit was sucuccessful!`;
+        feed.innerHTML = `${playerOne.name}'s hit was sucuccessful!`;
       }, "5000");
       setTimeout(() => {
-        playerTwoFeed.innerHTML = `${playerTwo.name[i]} health is now ${currentLife}`;
+        feed.innerHTML = `${playerTwo.name} health is now ${currentLife}`;
       }, "6000");
       setTimeout(() => {
         playerTwoHealth.innerHTML = `${currentLife}`;
       }, "6000");
+    } else {
       setTimeout(() => {
-        playerTwoFeed.innerHTML = `${playerTwo.name}'s attack was unsecessful!`;
+        feed.innerHTML = `${playerOne.name}'s attack was unsecessful!`;
       }, "3000");
       setTimeout(() => {
-        playerTwoFeed.innerHTML = `${playerOne.name} health is now ${currentLife}`;
+        feed.innerHTML = `${playerTwo.name} health is now ${currentLife}`;
       }, "5000");
       setTimeout(() => {
         playerOneHealth.innerHTML = `${currentLife}`;
@@ -106,19 +172,19 @@ const attackPlayerTwo = (player) => {
   playerTwo.turnToAttack = true;
 };
 
-const attackPlayerOne = (player) => {
-  let currentLife = playerTwo.health;
+const alienAttack = () => {
+  let currentLife = playerTwo.hull;
   if (playerTwo.turnToAttack === true) {
-    if (sucessfulHit < playerTwo.accuracy(1, 10)) {
-      currentLife = playerTwo.health -= playerOne.firepower(1, 10);
+    if (sucessfulHit < playerTwo.accuracy) {
+      currentLife = playerTwo.hull -= playerOne.firepower;
       setTimeout(() => {
-        playerOneFeed.innerHTML = `${playerTwo.name[i]} has fired!`;
+        feed.innerHTML = `${playerTwo.name} has fired!`;
       }, "2000");
       setTimeout(() => {
-        playerOneFeed.innerHTML = `${playerTwo.name[i]}'s hit was sucuccessful!`;
+        feed.innerHTML = `${playerTwo.name}'s hit was sucuccessful!`;
       }, "3000");
       setTimeout(() => {
-        playerOneFeed.innerHTML = `${playerOne.name} health is now ${currentLife}`;
+        feed.innerHTML = `${playerOne.name} health is now ${currentLife}`;
       }, "5000");
       setTimeout(() => {
         playerOneHealth.innerHTML = `${currentLife}`;
@@ -126,15 +192,15 @@ const attackPlayerOne = (player) => {
 
       if (currentLife <= 0) {
         setTimeout(() => {
-          playerOneFeed.innerHTML = `${playerTwo.name[i]} has lost the round`;
+          feed.innerHTML = `${playerTwo.name} has lost the round`;
         }, "1000");
       }
     } else {
       setTimeout(() => {
-        playerOneFeed.innerHTML = `${playerTwo.name[i]}'s attack was unsecessful!`;
+        feed.innerHTML = `${playerTwo.name}'s attack was unsecessful!`;
       }, "5000");
       setTimeout(() => {
-        playerOneFeed.innerHTML = `${playerTwo.name[i]} health is now ${currentLife}`;
+        feed.innerHTML = `${playerTwo.name} health is now ${currentLife}`;
       }, "6000");
       setTimeout(() => {
         playerOneHealth.innerHTML = `${currentLife}`;
@@ -145,36 +211,17 @@ const attackPlayerOne = (player) => {
   playerOne.turnToAttack = true;
 };
 
-const checkHP = (player) => {
-  if (playerOne.health <= 0) {
-    alert(`${playerOne.name[i]} has lost this round!`);
-    nextRound();
-  } else if (playerTwo.health <= 0) {
-    alert(`${playerTwo.name[i]} has lost this round!`);
-    nextRound();
-  }
+switchAlien = (playerTwo) => {
+  playerTwo = alienScum[counter];
+  alienImg.setAttribute("src", playerTwo.image);
+  playerTwoName.replaceWith = `${playerTwo.name}`;
+  playerTwoHealth.innerHTML = `${playerTwo.hull}`;
+  // feed.innerHTML = "NEW OPPONENT!";
+  // setTimeout(() => {
+  //   feed.innerHTML = `Now Battling ${playerTwo.name}`;
+  // }, "1000");
 };
 
-const nextRound = () => {
-  if (round <= 6) {
-    round += 1;
-    currentRound.innerHTML = `Round <br>${round}`;
-    if (i < playerTwo.name.length) {
-      i += 1;
-      playerTwo.name.innerHTML = `${playerTwo.name[i]}`;
-    }
-    // if (j < playerTwo.image.length) {
-    //   j += 1;
-    //   alien.setAttribute(src, playerTwo.image[j]);
-    // }
-  } else {
-    return "Game Over";
-  }
-  startRound();
+const resetGame = () => {
+  playerTwo = alienScum[0];
 };
-
-currentRound.innerHTML = `Round: <br>${round}`;
-playerOneName.innerHTML = `${playerOne.name}`;
-playerTwoName.innerHTML = `${playerTwo.name[i]}`;
-playerOneHealth.innerHTML = `${playerOne.health}`;
-playerTwoHealth.innerHTML = `${playerTwo.health}`;
